f3a909282553554130df5096788042b9ec73d6d3
ShiyamalanK <126349453+ShiyamalanK@users.noreply.github.com>
2024-09-22 19:55:45 +0530

    Update TodoList.js

--- components/TodoList.js
+++ components/TodoList.js
@@ -1,141 +1,31 @@
-import React, { useState } from 'react';
-import { View, TextInput,Text, TouchableOpacity,  ScrollView, StyleSheet } from 'react-native';
+import React from 'react';
+import { View, FlatList, StyleSheet } from 'react-native';
 import TodoItem from './TodoItem';
-import TabViewComp from './TabView';
+import { colors , colorList} from '../constants/colors';
 
-const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      padding: 10,
-      // backgroundColor: '#f2f2f2',
-    },
-    inputContainer: {
-      flexDirection: 'row',
-      alignItems: 'center',
-      marginTop: 15,
-      paddingHorizontal: 10,
-      paddingVertical: 5,
-      marginVertical: 15,
-      marginHorizontal: 10,
-      backgroundColor: '#fff',
-      borderRadius: 8,
-      borderWidth: 1,
-      borderColor: '#ddd',
-      shadowColor: '#000',
-      shadowOffset: { width: 0, height: 2 },
-      shadowOpacity: 0.1,
-      shadowRadius: 4,
-      elevation: 2,
-    },
-    textInput: {
-      flex: 1,
-      fontSize: 16,
-      padding: 10,
-      color: '#333',
-    },
-    addButton: {
-      marginLeft: 10,
-      backgroundColor: '#4CAF50',
-      paddingVertical: 10,
-      paddingHorizontal: 15,
-      borderRadius: 8,
-      width: 70,
-      alignItems: 'center',
-      justifyContent: 'center'
-    },
-    addButtonText: {
-      color: '#fff',
-      fontSize: 15,
-      fontWeight: 'bold'
-    },
-    tabContainer:{
-      flex: 1,
-      padding: 10,
-    }
-  });
-  
-
-export default function TodoList() {
-  // State Hooks
-  const [tasks, setTasks] = useState([
-    { id: 1, text: 'Doctor Appointment', completed: true },
-    { id: 2, text: 'Meeting at School', completed: false },
-  ]);
-  const [text, setText] = useState('');
-  // Function to Add Task
-  function addTask() {
-    const newTask = { id: Date.now(), text, completed: false };
-    setTasks([...tasks, newTask]);
-    setText('');
-  }
-  // Function to Delete Task
-  function deleteTask(id) {
-    setTasks(tasks.filter(task => task.id !== id));
-  }
-  // Function to Toggle Task Completion
-  function toggleCompleted(id) {
-    setTasks(tasks.map(task => (task.id === id ? { ...task, completed: !task.completed } : task)));
-  }
-  const CustomTabContent1 = () => (
-    <View style={styles.tabContainer}>
-      <ScrollView>
-
-      {tasks.filter(task => task.completed != true).map( task =>
-        <TodoItem 
-          key={task.id}
-          task={task}
-          deleteTask={deleteTask}
-          toggleCompleted={toggleCompleted}
-        />
-      )}
-      </ScrollView>
-    </View>
-  );
-  
-  const CustomTabContent2 = () => (
-    <View style={styles.tabContainer}>
-      <ScrollView>
-        {tasks.filter(task => task.completed == true).map( task =>
-        <TodoItem 
-          key={task.id}
-          task={task}
-          deleteTask={deleteTask}
-          toggleCompleted={toggleCompleted}
-        />
-      )}
-      </ScrollView>
-    </View>
-  );
-  // Render TodoList Component
+export default function TodoList({ tasks, onToggle, onDelete, onEdit }) {
   return (
     <View style={styles.container}>
-      <View style={styles.inputContainer}>
-        <TextInput
-          style={styles.textInput}
-          value={text}
-          onChangeText={setText}
-          placeholder="New Task"
-          placeholderTextColor="#999"
-        />
-        <TouchableOpacity style={styles.addButton} onPress={addTask}>
-          <Text style={styles.addButtonText}>Add</Text>
-        </TouchableOpacity>
-      </View>
-      <TabViewComp 
-        tabTitles={['Tasks', 'Completed']}
-        tabContent={[CustomTabContent1, CustomTabContent2]}
+      <FlatList
+        data={tasks}
+        renderItem={({ item }) => (
+          <TodoItem
+            task={item}
+            onToggle={() => onToggle(item.id)}
+            onDelete={() => onDelete(item.id)}
+            index = {tasks.indexOf(item)}
+            onEdit = {() => onEdit(item.id, item.name)}
+          />
+        )}
+        keyExtractor={(item) => item.id}
       />
-      {/* <ScrollView>
-      {tasks.map(task => (
-        <TodoItem
-        key={task.id}
-        task={task}
-        deleteTask={deleteTask}
-        toggleCompleted={toggleCompleted}
-        />
-      ))}
-      </ScrollView> */}
     </View>
-  
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.myWhite
+  },
+});
